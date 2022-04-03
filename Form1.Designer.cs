4e9bce51077e6ba6f09049114d2e955f3474e28f
FilipLjamic <92999337+FilipLjamic@users.noreply.github.com>
2022-04-03 12:54:53 +0200

    Nesto

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,13 +29,83 @@ namespace PodrumPica
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Naziv = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Cena = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Proizvodjac = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Procenat = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(131, 30);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Naziv,
+            this.Cena,
+            this.Proizvodjac,
+            this.Procenat});
+            this.dataGridView1.Location = new System.Drawing.Point(31, 56);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(443, 150);
+            this.dataGridView1.TabIndex = 2;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // Naziv
+            // 
+            this.Naziv.HeaderText = "Naziv";
+            this.Naziv.Name = "Naziv";
+            this.Naziv.ReadOnly = true;
+            // 
+            // Cena
+            // 
+            this.Cena.HeaderText = "Cena";
+            this.Cena.Name = "Cena";
+            this.Cena.ReadOnly = true;
+            // 
+            // Proizvodjac
+            // 
+            this.Proizvodjac.HeaderText = "Proizvodjac";
+            this.Proizvodjac.Name = "Proizvodjac";
+            this.Proizvodjac.ReadOnly = true;
+            // 
+            // Procenat
+            // 
+            this.Procenat.HeaderText = "% Alkohola";
+            this.Procenat.Name = "Procenat";
+            this.Procenat.ReadOnly = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Naziv;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Cena;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Proizvodjac;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Procenat;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -7,14 +7,54 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace PodrumPica
 {
     public partial class Form1 : Form
     {
+        DataTable tabela;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Load_Data()
+        {
+            SqlConnection veza = Konekcija.Connect();
+            SqlDataAdapter adapter = new SqlDataAdapter("SELECT * FROM pice", veza);
+            tabela = new DataTable();
+            adapter.Fill(tabela);
+        }
+
+        private void Ucitavanje()
+        {
+            if (tabela.Rows.Count != 0)
+            {
+                for (int i = 0; i < tabela.Rows.Count; i++)
+                {
+
+                    //listBox1.Items.Insert(i, );
+
+                    //listBox1.Items.AddRange(new object[] { "sdfwer, sdfas", "sdff" });
+                    //listBox1.Items.Add(tabela.Rows[i]["naziv"].ToString());
+                    //listBox1.Items.AddRange(string.Format(tabela.Rows[i]["naziv"].ToString(), tabela.Rows[i]["cena"].ToString()) );
+                    //listBox1.Items.Add(i, 1) = tabela.Rows[i]["naziv"].ToString();
+                    dataGridView1.Rows.Add(tabela.Rows[i]["naziv"].ToString(), tabela.Rows[i]["cena"].ToString(), tabela.Rows[i]["proizvodjac"].ToString(), tabela.Rows[i]["procenat_alkohola"].ToString());
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            Load_Data();
+            Ucitavanje();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
         }
     }
 }
